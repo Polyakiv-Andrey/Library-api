efcc32474cf54dbc4c50d20a06843c854706ac2b
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2023-01-27 22:59:21 +0200

    created model for book

--- book/models.py
+++ book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+cover = (
+    ("H", "HARD"),
+    ("S", "SOFT")
+)
+
+
+class Book(models.Model):
+    Title = models.CharField(max_length=255)
+    Author = models.CharField(max_length=255)
+    Cover = models.CharField(max_length=4, choices=cover)
+    Inventory = models.PositiveIntegerField()
+    Daily_fee = models.DecimalField()
+
+    def __str__(self):
+        return f"{self.Title}"
+
