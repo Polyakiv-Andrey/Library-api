54aec0d4000c1ec379dc0d41cc575226548e6de5
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2023-01-27 23:15:22 +0200

    created serializer for book

--- book/models.py
+++ book/models.py
@@ -11,7 +11,7 @@ class Book(models.Model):
     Author = models.CharField(max_length=255)
     Cover = models.CharField(max_length=4, choices=cover)
     Inventory = models.PositiveIntegerField()
-    Daily_fee = models.DecimalField()
+    Daily_fee = models.DecimalField(decimal_places=2, max_digits=8)
 
     def __str__(self):
         return f"{self.Title}"
